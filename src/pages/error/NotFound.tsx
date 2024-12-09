1bcf0ec275b038c972f92ee218bb082c19b75587
Islam Ahmed <71041347+Babywise@users.noreply.github.com>
2024-12-08 23:24:33 -0500

    Support for page routing

---
+++ src/pages/error/NotFound.tsx
@@ -0,0 +1,13 @@
+/**
+ * 404 page component.
+ */
+function NotFound() {
+  return (
+    <>
+      <h1>404: Not Found</h1>
+      <p>The page you are looking for does not exist.</p>
+    </>
+  );
+}
+
+export default NotFound;
