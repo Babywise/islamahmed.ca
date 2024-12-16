b739678ce7c77118b44a5ad6a147bfff080f386c
Islam Ahmed <71041347+Babywise@users.noreply.github.com>
2024-12-16 12:52:21 -0500

    IA-12 Simple footer added

---
+++ src/components/footer/Footer.tsx
@@ -0,0 +1,12 @@
+/**
+ * Footer component.
+ */
+function Footer() {
+  return (
+    <footer className="bg-general-60/25 text-general-10/75 py-4 text-center">
+      <p>&copy; 2024 Islam Ahmed. All rights reserved.</p>
+    </footer>
+  );
+}
+
+export default Footer;
