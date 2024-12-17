5364f469df9755826dc2b90a407dac19e494d1a9
Islam Ahmed <71041347+Babywise@users.noreply.github.com>
2024-12-17 17:53:30 -0500

    IA-8 Added px ref to tailwind fontsize config

--- tailwind.config.js
+++ tailwind.config.js
@@ -15,12 +15,12 @@ export default {
         }
       },
       fontSize: {
-        xs: ["0.75rem", { lineHeight: "1rem" }],
-        sm: ["0.875rem", { lineHeight: "1.25rem" }],
-        base: ["1rem", { lineHeight: "1.5rem" }],
-        lg: ["1.125rem", { lineHeight: "1.75rem" }],
-        xl: ["1.25rem", { lineHeight: "1.75rem" }],
-        "2xl": ["1.5rem", { lineHeight: "2rem" }]
+        xs: ["0.75rem", { lineHeight: "1rem" }], // 12px
+        sm: ["0.875rem", { lineHeight: "1.25rem" }], // 14px
+        base: ["1rem", { lineHeight: "1.5rem" }], // 16px
+        lg: ["1.125rem", { lineHeight: "1.75rem" }], // 18px
+        xl: ["1.25rem", { lineHeight: "1.75rem" }], // 20px
+        "2xl": ["1.5rem", { lineHeight: "2rem" }] // 24px
       },
       fontFamily: {
         roboto: ["Roboto", "sans-serif"],
