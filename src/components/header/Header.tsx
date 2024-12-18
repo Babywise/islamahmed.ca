fdaa9ca7f6e5a1b54ecf229f72d7389766070810
Islam Ahmed <71041347+Babywise@users.noreply.github.com>
2024-12-18 04:44:03 -0500

    IA-21 Added accessibility labels to header elements

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -7,8 +7,13 @@ function Header() {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
 
   return (
-    <header className="sticky top-0 flex h-[56px] items-center justify-center overflow-x-hidden bg-general-60/25 font-bold tracking-widest text-general-10/75">
-      <nav className="flex size-full items-center justify-between">
+    <header
+      aria-label="Header"
+      className="sticky top-0 flex h-[56px] items-center justify-center overflow-x-hidden bg-general-60/25 font-bold tracking-widest text-general-10/75">
+      {/* Navbar */}
+      <nav
+        aria-label="Navbar"
+        className="flex size-full items-center justify-between">
         <img
           alt="Logo"
           className="aspect-square h-full object-contain p-[5px]"
@@ -17,6 +22,7 @@ function Header() {
 
         {/* Hamburger button for mobile */}
         <button
+          aria-label="Toggle menu"
           className="p-2 sm:hidden"
           onClick={() => {
             setIsMenuOpen(!isMenuOpen);
@@ -31,15 +37,29 @@ function Header() {
 
         {/* Navigation menu */}
         <ul
-          className={`fixed right-0 top-[56px] w-full transform bg-general-60/95 p-4 transition-none ${isMenuOpen ? "" : "translate-x-full"} sm:relative sm:top-0 sm:flex sm:w-auto sm:translate-x-0 sm:items-center sm:gap-4 sm:bg-transparent sm:p-0`}>
-          <li className="flex justify-center py-2 hover:underline sm:py-0">
-            <a href={`${import.meta.env.BASE_URL}/`}>Home</a>
+          aria-label="Navigation menu"
+          className={`fixed right-0 top-[56px] w-full transform bg-general-60/95 p-4 transition-none ${isMenuOpen ? "" : "translate-x-full"} sm:relative sm:top-0 sm:flex sm:w-auto sm:translate-x-0 sm:items-center sm:gap-4 sm:bg-transparent sm:p-0`}
+          role="menubar">
+          <li
+            className="flex justify-center py-2 hover:underline sm:py-0"
+            role="none">
+            <a href={`${import.meta.env.BASE_URL}/`} role="menuitem">
+              Home
+            </a>
           </li>
-          <li className="flex justify-center py-2 hover:underline sm:py-0">
-            <a href={`${import.meta.env.BASE_URL}/projects`}>Projects</a>
+          <li
+            className="flex justify-center py-2 hover:underline sm:py-0"
+            role="none">
+            <a href={`${import.meta.env.BASE_URL}/projects`} role="menuitem">
+              Projects
+            </a>
           </li>
-          <li className="flex justify-center py-2 hover:underline sm:py-0">
-            <a href={`${import.meta.env.BASE_URL}/about`}>About</a>
+          <li
+            className="flex justify-center py-2 hover:underline sm:py-0"
+            role="none">
+            <a href={`${import.meta.env.BASE_URL}/about`} role="menuitem">
+              About
+            </a>
           </li>
         </ul>
       </nav>
