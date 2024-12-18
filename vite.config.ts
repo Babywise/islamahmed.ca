5869f77a860fc3a7e7032401c3fb9c7f3f76fd65
Islam Ahmed <71041347+Babywise@users.noreply.github.com>
2024-12-18 13:34:19 -0500

    IA-22 Removed repo prefix from vite base

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/islamahmed.ca",
+  base: "/",
   build: {
     outDir: "./dist",
     target: ["chrome90", "edge90", "es2022", "firefox89", "safari15"]
