69c1a155e3bbf57005b1924d40cae608829bd497
Islam Ahmed <71041347+Babywise@users.noreply.github.com>
2024-12-29 22:53:44 -0500

    IA-53 added sections to the homepage

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -3,30 +3,44 @@
  */
 function Home() {
   return (
-    <div
-      className="flex flex-col items-center justify-center text-center tracking-widest"
-      id="home-landing"
-      role="none">
-      <div
-        className="text-xl sm:text-4xl"
-        id="landing-heading"
-        role="presentation">
-        <h1>
-          Hello, I&apos;m{" "}
-          <span className="font-bold text-general-40">Islam</span>.
-        </h1>
-        <h1>I&apos;m a full stack web developer.</h1>
-      </div>
-      <button
-        className="mt-4 w-1/3 min-w-48"
-        onClick={() => {
-          const element = document.getElementById("start");
-          element?.scrollIntoView({ behavior: "smooth" });
-        }}
-        type="button">
-        <span>Begin the Tour</span>
-      </button>
-    </div>
+    <>
+      {/* <!-- Hero section --> */}
+      <section id="hero">
+        <div
+          className="flex min-h-[calc(100vh-56px)] flex-col items-center justify-center text-center tracking-widest"
+          id="hero-content"
+          role="none">
+          <div
+            className="text-xl sm:text-4xl"
+            id="hero-heading"
+            role="presentation">
+            <h1>
+              Hello, I&apos;m{" "}
+              <span className="font-bold text-general-40">Islam</span>.
+            </h1>
+            <h1>I&apos;m a full stack web developer.</h1>
+          </div>
+          <button
+            className="mt-4 w-1/3 min-w-48"
+            onClick={() => {
+              const element = document.getElementById("start");
+              element?.scrollIntoView({ behavior: "smooth" });
+            }}
+            type="button">
+            <span>Begin the Tour</span>
+          </button>
+        </div>
+      </section>
+      {/* <!-- Start section --> */}
+      <section id="start">
+        <div
+          className="flex flex-col tracking-widest"
+          id="start-content"
+          role="none">
+          <h1 className="mb-4 text-2xl font-bold tracking-widest">Start</h1>
+        </div>
+      </section>
+    </>
   );
 }
 
