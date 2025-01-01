33d0b0c0ed3901fe62f975e7af4394b30fc0abc1
Islam Ahmed <71041347+Babywise@users.noreply.github.com>
2025-01-01 15:36:43 -0500

    IA-59 footer css moved

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,17 +1,16 @@
+import "./Footer.css";
+
 /**
  * Footer component.
  */
 function Footer() {
   return (
-    <footer
-      aria-label="Footer"
-      className="flex h-[56px] items-center justify-center">
-      <section id="copyright">
-        <h1 className="sr-only">Copyright Notice</h1>
+    <footer aria-label="Footer">
+      <div className="footer-content">
         <p aria-label="Copyright">
-          <small>&copy; 2024 Islam Ahmed. All rights reserved.</small>
+          &copy; 2024 Islam Ahmed. All rights reserved.
         </p>
-      </section>
+      </div>
     </footer>
   );
 }
